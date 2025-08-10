221b71025603a68a2e566ab5c00c48b69e636b5e
zzeen2 <jking120393@gmail.com>
2025-08-10 20:59:36 +0900

    [build] drizzle db 환경 세팅

--- apps/backend/src/README.md
+++
@@ -1,5 +0,0 @@
-## 폴더형태만드릭
-
-nest g resource 폴더명 -> 서비스 컨트롤러 dto 모델 다만들어짐 
-
-npm install drizzle-orm -D drizzle-kit pg;
--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
