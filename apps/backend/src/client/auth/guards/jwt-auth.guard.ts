ffe5181b0269e97d90569f006fc61928310cd28e
zzeen2 <jking120393@gmail.com>
2025-09-15 10:16:47 +0900

    apps/backend 폴더 제거

--- apps/backend/src/client/auth/guards/jwt-auth.guard.ts
+++
@@ -1,2 +0,0 @@
-import { AuthGuard } from '@nestjs/passport';
-export class JwtAuthGuard extends AuthGuard('jwt') {}
--- apps/backend/src/client/musics/musics.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MusicsService } from './musics.service';
-import { MusicsController } from './musics.controller';
-
-@Module({
-  controllers: [MusicsController],
-  providers: [MusicsService],
-})
-export class MusicsModule {}
